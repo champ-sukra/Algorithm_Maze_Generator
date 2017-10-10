b65f0a4b47055366a1de8e22b656237f6d382c4b
Chaithat Sukra <chaithatsukra@Chaithats-MacBook-Pro.local>
2017-10-10 13:09:14 +1100

    a

---
+++ src/mazeGenerator/RecursiveBacktrackerGenerator.java
@@ -0,0 +1,14 @@
+package mazeGenerator;
+
+import maze.Maze;
+
+public class RecursiveBacktrackerGenerator implements MazeGenerator {
+
+	@Override
+	public void generateMaze(Maze maze) {
+		
+		System.out.println(maze.toString());
+
+	} // end of generateMaze()
+
+} // end of class RecursiveBacktrackerGenerator
