f2632fa294cedd48bf691553c450491ef6cac0e7
Chaithat Sukra <iChaithat@Chaithats-MacBook-Pro-4.local>
2017-10-13 05:38:47 +1100

    - Complete recursive backtracker for Bi - Start implementing Bi

--- src/mazeSolver/BiDirectionalRecursiveBacktrackerSolver.java
+++ src/mazeSolver/BiDirectionalRecursiveBacktrackerSolver.java
@@ -13,59 +13,65 @@ import maze.Wall;
  * Implements the BiDirectional recursive backtracking maze solving algorithm.
  */
 public class BiDirectionalRecursiveBacktrackerSolver implements MazeSolver {
-
-	private boolean[][] markedEn;
-	private boolean[][] markedEx;
+	
+	private boolean nextStep = false;
+	private int step = 0;
+	
+	private interface ICallbacks { 	    
+	    public void onWalked(Cell aEntrance, boolean[][] aMarked, ICallbacks aBlock);
+	}
 	
 	@Override
 	public void solveMaze(Maze maze) {
 
-		this.initMarked(maze.sizeR, maze.sizeC);
+		boolean[][] markedEn = new boolean[maze.sizeR][maze.sizeC];
+		boolean[][] markedEx = new boolean[maze.sizeR][maze.sizeC];
 		
-		Cell entrance = maze.entrance;		
-		this.markedEn[entrance.r][entrance.r] = true;
-		this.performDfsForEntrance(entrance);
+		this.initMarked(markedEn, maze.sizeR, maze.sizeC);
+		this.initMarked(markedEx, maze.sizeR, maze.sizeC);
 		
-		Cell exit = maze.exit;
-		this.markedEn[entrance.r][entrance.r] = true;
-//		this.performDfsForExit(exit);
+		Cell entrance = maze.entrance;
+		Cell exit = maze.exit;		
 		
+		this.performDfs(entrance, exit, maze, markedEn, markedEx, true);
 	} // end of solveMaze()
 
-	private void performDfsForEntrance(Cell aEntrance) {
-		//Find direction
+
+	
+	public void performDfs(Cell aEntrance1, Cell aEntrance2, Maze aMaze, boolean[][] aMarked1, boolean[][] aMarked2, boolean aIsNewMoves) {
 		
+		Cell aEntrance = (aIsNewMoves == true) ? aEntrance1 : aEntrance2; 
+		boolean[][] aMarked = (aIsNewMoves == true) ? aMarked1 : aMarked2;		
+		
+		//Getting not present wall
 		HashMap<Integer, Wall> walls = new HashMap<Integer, Wall>();		
-		for (int i = 0; i < walls.size(); i++) {
+		for (int i = 0; i < aEntrance.wall.length; i++) {
 			Wall wall = aEntrance.wall[i];
 			if (wall != null && wall.present == false) {
 				walls.put(new Integer(i), wall);
 			}
 		}
+		//Shuffling
 		List<Integer> keys = new ArrayList<Integer>(walls.keySet());
 		Collections.shuffle(keys);
-		
-		int direction = keys.get(0);
-		Wall selectedWall = walls.get(direction);
-		Cell goTo = aEntrance.neigh[direction];
-	}
-	
-	private void performDfsForExit() {
-		
-	}
+					
+		for (int direction : keys) {
+			Cell goTo = aEntrance.neigh[direction];								
+			if (!aMarked[goTo.r][goTo.c]) {					
+				
+				aMarked1[aEntrance.r][aEntrance.c] = true;
+				aMaze.drawFtPrt(aEntrance);
+				
+				aIsNewMoves = !aIsNewMoves;
+				this.performDfs((aIsNewMoves == false)? goTo : aEntrance1, (aIsNewMoves == false)? aEntrance2 : goTo, aMaze, aMarked1, aMarked2, aIsNewMoves);
+			}	
+		}		
+	}	
 
-	private void initMarked(int aSizeR, int aSizeC) {
-    	this.markedEn = new boolean[aSizeR][aSizeC];
+	private void initMarked(boolean[][] aMarked, int aSizeR, int aSizeC) {
 		for (int i = 0; i < aSizeR; i++) {
 			for (int j = 0; j < aSizeC; j++) {
-				markedEn[i][j] = false;
-			}
-		}	
-		
-		this.markedEx = new boolean[aSizeR][aSizeC];
-		for (int i = 0; i < aSizeR; i++) {
-			for (int j = 0; j < aSizeC; j++) {
-				markedEx[i][j] = false;
+				aMarked[i][j] = false;
 			}
 		}	
     }
